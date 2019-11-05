184a6e612f6d683e0e24fb3af01a8af0a9c03d1c
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-05 11:34:52 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const checkExistingEmail = await User.findOne({ where: { email } });
+
+    if (checkExistingEmail) {
+      return res.status(400).json({ error: 'Email already exists.' });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+}
+
+export default new UserController();
